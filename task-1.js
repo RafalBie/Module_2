2dc08df87a87f9e98c34ee45f268478e0e960e9c
Rafal Bie <rafalbie@mbp-rafal.home>
2020-04-03 15:31:18 +0200

    commit

---
+++ task-1.js
@@ -0,0 +1,14 @@
+// 1) Stwórz strukturę danych związaną z książką adresową.
+
+// Obiekt "książka adresowa" 
+// Ma mieć: listę wszystkich kontaktów, listę grup kontaktów.
+// Ma umożliwiać: create/read/update/delete , umożliwiać sortowanie listy po frazach
+
+// Obiekt charakteryzujący pojedyńczy kontak:
+// Ma mieć: Imie, Nazwisko, adres-emial, datę modyfikacji
+// Ma umożliwiać: Tworzenie obiektu, aktualizację datę modyfikacji, wyświetlać 
+// w odpowiednim formacie przy wywołaniu. 
+
+// Obiekt charakteryzujący grupę kontaktów:
+// Ma mieć: listę kontaktów
+// Ma umożliwiać: Create/Read/Update/Remove (CRUD)
