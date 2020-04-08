a987482a6327812837446f86f4ecf2f78b507b24
Rafal Bie <rafalbie@mbp-rafal.home>
2020-04-08 11:21:13 +0200

    commit

--- task-1.js
+++ task-1.js
@@ -1,4 +1,35 @@
 // 1) Stwórz strukturę danych związaną z książką adresową.
+// Obiekt charakteryzujący pojedyńczy kontak:
+// Ma mieć: Imie, Nazwisko, adres-emial, datę modyfikacji
+// Ma umożliwiać: Tworzenie obiektu, aktualizację datę modyfikacji, wyświetlać 
+// w odpowiednim formacie przy wywołaniu. 
+
+class Contact {
+    constructor(name, surname, email, date){
+        this.name = name;
+        this.surname = surname;
+        this.email= email;
+        this.date = date;
+    }
+    createContact(newContact){
+        this.name.push(newContact)
+}
+}
+const Kowalski2 = new Contact ('jan','kowalski2','jw@op.pl','121')
+
+// Obiekt charakteryzujący grupę kontaktów:
+// Ma mieć: listę kontaktów
+// Ma umożliwiać: Create/Read/Update/Remove (CRUD)
+class GroupContact extends Contact{
+    constructor(name, surname,email, date){
+        super(name,surname,email,date)
+        this.contactList = contactList
+    }
+    read(){
+        
+    }
+}
+
 
 // Obiekt "książka adresowa" 
 // Ma mieć: listę wszystkich kontaktów, listę grup kontaktów.
@@ -17,12 +48,5 @@ class AdressBook {
 const Kowalski = new AdressBook ('Kowalski')
 
 
-// Obiekt charakteryzujący pojedyńczy kontak:
-// Ma mieć: Imie, Nazwisko, adres-emial, datę modyfikacji
-// Ma umożliwiać: Tworzenie obiektu, aktualizację datę modyfikacji, wyświetlać 
-// w odpowiednim formacie przy wywołaniu. 
 
-// Obiekt charakteryzujący grupę kontaktów:
-// Ma mieć: listę kontaktów
-// Ma umożliwiać: Create/Read/Update/Remove (CRUD)
 
